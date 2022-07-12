ce57843094561f1c35621b2379c873223ca32412
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-12 20:27:26 +0530

    Modified display.js

--- modules/Books.js
+++ modules/Books.js
@@ -4,15 +4,20 @@
      addBooks() {
         const name = document.getElementById('title').value;
         const author = document.getElementById('author').value;
-        this.booklist.push({ title: name, author });
+        this.booklist.push({ title: name, author: author });
         localStorage.setItem('books', JSON.stringify(this.booklist));
         history.back();
         location.reload();
     }
+
+    deleteBook(i) {
+        // eslint-disable-next-line no-use-before-define
+        this.booklist.splice(i, 1);
+        localStorage.setItem('books', JSON.stringify(this.booklist));
+        location.reload();
    
         
 }
+ }
 
-const book = new Books();
-export {book};
- 
+export const book = new Books();
--- modules/Display.js
+++ modules/Display.js
@@ -1,12 +1,5 @@
-import {book} from './Books.js'
-function deleteBook(i) {
-    // eslint-disable-next-line no-use-before-define
-    book.booklist.splice(i, 1);
-    localStorage.setItem('books', JSON.stringify(book.booklist));
-    location.reload();
-  }
-
-export const showBookList = () => {
+import {book} from './Books.js';
+ export const showBookList = () => {
     const storedBook = JSON.parse(localStorage.getItem('books'));
     let html = '<div>';
     html += '<table class="center">';
@@ -15,11 +8,11 @@ export const showBookList = () => {
       html += '<tr class="rows">';
       html += `<td> "${book.booklist[i].title}" by ${book.booklist[i].author}</td>`;
       html += `<td class="test"><button class="remove-btn" id="removebtn"
-      onclick="${deleteBook(i)}"> Remove </button> </td>`;
+      onclick="book.deleteBook(${i})"> Remove </button> </td>`;
       html += '</tr>';
       document.getElementById('list').innerHTML = html;
     }
     html += '</table>';
   }
-  export {book};
-  showBookList();
+
+export {book} from './Books.js';
