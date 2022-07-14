b9bafd27eb956bcbf5bd0b39c43f7b27ee4c0021
Riyaz Ahmed <r-ahmed@riysaim.com>
2022-07-14 14:25:40 +0530

    fixed suggested errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
-import Books from './Books.js';
-import { DateTime } from './luxon.min.js';
-import showBookList from './Display.js';
+import Books from './modules/Books.js';
+import { DateTime } from './modules/luxon.min.js';
+import showBookList from './modules/Display.js';
 
 const book = new Books();
 const button = document.getElementById('addbook');
@@ -10,7 +9,6 @@ button.addEventListener('click', () => {
 });
 
 window.deleteBook = (i) => {
-  // eslint-disable-next-line no-use-before-define
   book.booklist.splice(i, 1);
   localStorage.setItem('books', JSON.stringify(book.booklist));
   window.location.reload();
@@ -21,8 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const now = DateTime.now().toJSDate();
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const monthName = months[now.getMonth()];
 setInterval(() => {
   document.getElementById('datefield').innerHTML = now;
 }, 1000);
